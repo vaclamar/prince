a1837405e793c91a33d0fcc6fa22f1eb26109ce7
Martin Vaclavik <martin.vaclavik@teliasonera.com>
2015-12-03 11:18:34 +0100

    First step

--- src/main/java/vm/VmStrategy.java
+++ src/main/java/vm/VmStrategy.java
@@ -1,9 +1,11 @@
+package vm;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vm;
+//package vma;
 
 
 import cz.yellen.xpg.common.GameStrategy;
