6d06f4be2deee7e9236dc67ef160f05fa0d90b24
Vít Sykala <vit.sykala@telia.dk>
2015-12-04 11:40:25 +0100

    level4Final
    Signed-off-by: Vít Sykala <vit.sykala@telia.dk>

--- src/main/java/princeGame/MyGameStrategy.java
+++ src/main/java/princeGame/MyGameStrategy.java
@@ -1,7 +1,5 @@
 package princeGame;
 
-import java.util.Set;
-
 import cz.yellen.xpg.common.GameStrategy;
 import cz.yellen.xpg.common.action.Action;
 import cz.yellen.xpg.common.action.Direction;
@@ -13,210 +11,219 @@ import cz.yellen.xpg.common.action.Use;
 import cz.yellen.xpg.common.stuff.GameObject;
 import cz.yellen.xpg.common.stuff.GameSituation;
 
+import java.util.Set;
+
+import static java.lang.Integer.parseInt;
+
 public class MyGameStrategy implements GameStrategy {
 
-	private GameObject prince;
-	private GameObject sword;
-    private GameObject bottle;
-	private Set<GameObject> gameObjects;
-	private Action action;
-	private Direction direction = Direction.FORWARD;
+    private GameObject prince;
+    private GameObject sword;
+    private Set<GameObject> gameObjects;
+    private Action action;
+    private Direction direction = Direction.FORWARD;
+    private GameObject inventoryBottle;
+    private GameSituation gameSituation;
 
-	public Action step(GameSituation situation) {
+    public Action step(GameSituation situation) {
 
+        this.gameSituation = situation;
+        action = null;
+        gameObjects = situation.getGameObjects();
+        prince = findPrince();
+        if (prince.getProperty("health").equals("1")) {
+            return new Use(inventoryBottle, prince);
+        }
 
-		action = null;
-		gameObjects = situation.getGameObjects();
-		prince = findPrince();
-        if(prince.getProperty("health").equals("1")) {
-            for ( GameObject go : prince.getStuff()) {
-                if(go.getType().equals("bottle")) {
-                    return new Use(bottle, prince);
+        if (gameObjects.size() == 1) {
+            return move();
+        } else {
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("gate")) {
+                    action = actionForGate(gameObject);
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
+                }
+            }
+
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("sword")) {
+                    action = actionForSword(gameObject);
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
+                }
+            }
+
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("bottle")) {
+                    action = actionForBottle(gameObject);
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
+                }
+            }
+
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("guard") && gameObject.getProperties().get("dead").equals("false")) {
+                    if (parseInt(prince.getProperties().get("health")) > parseInt(gameObject.getProperties().get("health"))) {
+                        action = actionForGuard(gameObject);
+                    } else {
+                        changeDirection();
+                    }
+
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
+                }
+            }
+
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("pit")) {
+                    action = actionForPit(gameObject);
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
+                }
+            }
+
+            for (GameObject gameObject : gameObjects) {
+                if (gameObject.getType().equals("wall")) {
+                    action = actionForWall(gameObject);
+                    if (action == null) {
+                        continue;
+                    } else {
+                        return action;
+                    }
                 }
             }
         }
 
-		if (gameObjects.size() == 1) {
-			return move();
-		} else {
-			for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("gate")) {
-					action = actionForGate(gameObject);
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
-			
-			for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("sword")) {
-					action = actionForSword(gameObject);
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
+        return move();
+    }
 
-           for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("bottle")) {
-					action = actionForBottle(gameObject);
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
-			
-			for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("guard") && gameObject.getProperties().get("dead").equals("false")) {
-					if(Integer.parseInt(prince.getProperties().get("health")) > Integer.parseInt(gameObject.getProperties().get("health"))){
-						action = actionForGuard(gameObject);
-					} else {
-						changeDirection();
-					}
-						
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
+    // ACTIONS FOR OBJECTS
 
-			for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("pit")) {
-					action = actionForPit(gameObject);
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
-
-			for (GameObject gameObject : gameObjects) {
-				if (gameObject.getType().equals("wall")) {
-					action = actionForWall(gameObject);
-					if (action == null) {
-						continue;
-					} else {
-						return action;
-					}
-				}
-			}
-		}
-		
-		return move();
-	}
-
-	// ACTIONS FOR OBJECTS
-	
-	private Action actionForSword(GameObject gameObject) {
-		if (gameObject.getPosition() == prince.getPosition()) {
-			return pickUp(gameObject);
-		}
-		return null;
-	}
+    private Action actionForSword(GameObject gameObject) {
+        if (gameObject.getPosition() == prince.getPosition()) {
+            return pickUp(gameObject);
+        }
+        return null;
+    }
 
     private Action actionForBottle(GameObject gameObject) {
-		if (gameObject.getPosition() == prince.getPosition()) {
-			return pickUp(gameObject);
-		}
-		return null;
-	}
+        if (gameObject.getPosition() == prince.getPosition() && !gameObject.getProperty("odour").equals("puke")) {
+            return pickUp(gameObject);
+        }
+        return null;
+    }
 
-	private Action actionForGuard(GameObject gameObject) {
-		if (direction == Direction.FORWARD && gameObject.getPosition() == prince.getPosition() + 1) {
-			for (GameObject stuff : prince.getStuff()) {
-				if (stuff.getType().equals("sword")) {
-					sword = stuff;
-					return attack(gameObject);
-				} else {
-					changeDirection();
-					return null;
-				}
-			}
-		}else if (direction == Direction.BACKWARD && gameObject.getPosition() == prince.getPosition() - 1) {
-			for (GameObject stuff : prince.getStuff()) {
-				if (stuff.getType().equals("sword")) {
-					sword = stuff;
-					return attack(gameObject);
-				} else {
-					changeDirection();
-					return null;
-				}
-			}
-		}
-		
-		return null;
-	}
+    private Action actionForGuard(GameObject guard) {
+        if (direction == Direction.FORWARD && guard.getPosition() == prince.getPosition() + 1) {
+            for (GameObject stuff : prince.getStuff()) {
+                if (stuff.getType().equals("sword") &&
+                        parseInt(guard.getProperty("health")) < parseInt(prince.getProperty("health")) + parseInt(inventoryBottle.getProperty("volume")) - 1) {
+                    sword = stuff;
+                    return attack(guard);
+                } else {
+                    changeDirection();
+                    return null;
+                }
+            }
+        } else if (direction == Direction.BACKWARD && guard.getPosition() == prince.getPosition() - 1) {
+            for (GameObject stuff : prince.getStuff()) {
+                if (stuff.getType().equals("sword")) {
+                    sword = stuff;
+                    return attack(guard);
+                } else {
+                    changeDirection();
+                    return null;
+                }
+            }
+        }
 
-	private Action actionForPit(GameObject gameObject) {
-		if (direction == Direction.FORWARD && gameObject.getPosition() == prince.getPosition() + 1) {
-			return jump();
-		} else if (direction == Direction.BACKWARD && gameObject.getPosition() == prince.getPosition() - 1) {
-			return jump();
-		}
-		return null;
-	}
+        return null;
+    }
 
-	private Action actionForGate(GameObject gameObject) {
-		if (gameObject.getPosition() == prince.getPosition()) {
-			return enterGate(gameObject);
-		} else {
-			return null;
-		}
-	}
+    private Action actionForPit(GameObject gameObject) {
+        if (direction == Direction.FORWARD && gameObject.getPosition() == prince.getPosition() + 1) {
+            return jump();
+        } else if (direction == Direction.BACKWARD && gameObject.getPosition() == prince.getPosition() - 1) {
+            return jump();
+        }
+        return null;
+    }
 
-	private Action actionForWall(GameObject gameObject) {
-		if (direction == Direction.FORWARD && gameObject.getPosition() == prince.getPosition() + 1) {
-			changeDirection();
-		} else if (direction == Direction.BACKWARD && gameObject.getPosition() == prince.getPosition() - 1) {
-			changeDirection();
-		}
-		return null;
-	}
+    private Action actionForGate(GameObject gameObject) {
+        if (gameObject.getPosition() == prince.getPosition()) {
+            return enterGate(gameObject);
+        } else {
+            return null;
+        }
+    }
 
-	private GameObject findPrince() {
-		for (GameObject gameObject : gameObjects) {
-			if (gameObject.getType().equals("prince")) {
-				return gameObject;
-			}
-		}
-		return null;
-	}
+    private Action actionForWall(GameObject gameObject) {
+        if (direction == Direction.FORWARD && gameObject.getPosition() == prince.getPosition() + 1) {
+            changeDirection();
+        } else if (direction == Direction.BACKWARD && gameObject.getPosition() == prince.getPosition() - 1) {
+            changeDirection();
+        }
+        return null;
+    }
 
-	private void changeDirection() {
-		if (direction == Direction.FORWARD) {
-			direction = Direction.BACKWARD;
-		} else {
-			direction = Direction.FORWARD;
-		}
-	}
+    private GameObject findPrince() {
+        for (GameObject gameObject : gameObjects) {
+            if (gameObject.getType().equals("prince")) {
+                if(gameObject.getStuff()!=null){
+                    for (GameObject staff : gameObject.getStuff()) {
+                        if (staff.getType().equals("bottle")) {
+                            inventoryBottle = staff;
+                        }
+                    }
+                }
+                return gameObject;
+            }
+        }
+        return null;
+    }
 
-	// PRINCE ACTIONS
-	private Action enterGate(GameObject gameObject) {
-		return new Enter(gameObject);
-	}
+    private void changeDirection() {
+        if (direction == Direction.FORWARD) {
+            direction = Direction.BACKWARD;
+        } else {
+            direction = Direction.FORWARD;
+        }
+    }
 
-	private Action move() {
-		return new Move(direction);
-	}
+    // PRINCE ACTIONS
+    private Action enterGate(GameObject gameObject) {
+        return new Enter(gameObject);
+    }
 
-	private Action jump() {
-		return new Jump(direction);
-	}
-	
-	private Action attack(GameObject enemy) {
-		return new Use(sword, enemy);
-	}
-	
-	private Action pickUp(GameObject item){
-		return new PickUp(item);
-	}
+    private Action move() {
+        return new Move(direction);
+    }
+
+    private Action jump() {
+        return new Jump(direction);
+    }
+
+    private Action attack(GameObject enemy) {
+        return new Use(sword, enemy);
+    }
+
+    private Action pickUp(GameObject item) {
+        return new PickUp(item);
+    }
 }
